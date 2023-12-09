6058975e8f87b17e54fea9b1477844f6ee1029a2
Danielle Church <dani.church@gmail.com>
2023-12-08 19:49:38 -0500

    Initial effort at mobile UI

--- views/timecontrols.view.js
+++ views/timecontrols.view.js
@@ -14,26 +14,28 @@ Views.registerView("timeControls", {
     html() {
     // todo: add talent tree
         const html =
-        `<button id='pausePlay' onclick='pauseGame()'' class='button control'>${_txt("time_controls>pause_button")}</button>
-        <button onclick='manualRestart()' class='button showthatO control'>${_txt("time_controls>restart_button")}
-            <div class='showthis' style='color:black;width:230px;'>${_txt("time_controls>restart_text")}</div>
-        </button>
-        <button class='button showthatO control' onclick='toggleOffline()'>${_txt("time_controls>bonus_seconds>title")}
-            <div class='showthis' style='width:230px;color:black;'>${_txt("time_controls>bonus_seconds>main_text")}
-                <div class='bold' id='isBonusOn'>${_txt("time_controls>bonus_seconds>state>off")}</div><br>
-                <div class='bold'>${_txt("time_controls>bonus_seconds>counter_text")}</div> <div id='bonusSeconds'></div>
-            </div>
-        </button>
-        <button id='talentTreeBtn' style='display: none;' onclick='view.showTalents()'' class='button control'>${_txt("time_controls>talents_button")}</button>
-        <div class='control'>
-            <div tabindex='0' id='story_control' class='showthatO' onmouseover='view.updateStory(storyShowing)' onfocus='view.updateStory(storyShowing)' style='height:30px;'>
-                <div class='large bold'>${_txt("time_controls>story_title")}</div>
-                <div id='newStory' style='color:red;display:none;'>(!)</div>
-                <div id='story_tooltip' class='showthisH' style='width:400px;'>
-                    <button style='margin-left:175px;' class='actionIcon fa fa-arrow-left control' id='storyLeft' onclick='view.updateStory(storyShowing-1)'></button>
-                    <div style='' id='storyPage' class='bold control'></div>
-                    <button style='' class='actionIcon fa fa-arrow-right control' id='storyRight' onclick='view.updateStory(storyShowing+1)'></button>
-                    ${this.stories()}
+        `<div id='timeControlsMain'>
+            <button id='pausePlay' onclick='pauseGame()'' class='button control'>${_txt("time_controls>pause_button")}</button>
+            <button onclick='manualRestart()' class='button showthatO control'>${_txt("time_controls>restart_button")}
+                <div class='showthis' style='color:black;width:230px;'>${_txt("time_controls>restart_text")}</div>
+            </button>
+            <button class='button showthatO control' onclick='toggleOffline()'>${_txt("time_controls>bonus_seconds>title")}
+                <div class='showthis' style='width:230px;color:black;'>${_txt("time_controls>bonus_seconds>main_text")}
+                    <div class='bold' id='isBonusOn'>${_txt("time_controls>bonus_seconds>state>off")}</div><br>
+                    <div class='bold'>${_txt("time_controls>bonus_seconds>counter_text")}</div> <div id='bonusSeconds'></div>
+                </div>
+            </button>
+            <button id='talentTreeBtn' style='display: none;' onclick='view.showTalents()'' class='button control'>${_txt("time_controls>talents_button")}</button>
+            <div class='control'>
+                <div tabindex='0' id='story_control' class='showthatO' onmouseover='view.updateStory(storyShowing)' onfocus='view.updateStory(storyShowing)' style='height:30px;'>
+                    <div class='large bold'>${_txt("time_controls>story_title")}</div>
+                    <div id='newStory' style='color:red;display:none;'>(!)</div>
+                    <div id='story_tooltip' class='showthisH' style='width:400px;'>
+                        <button style='margin-left:175px;' class='actionIcon fa fa-arrow-left control' id='storyLeft' onclick='view.updateStory(storyShowing-1)'></button>
+                        <div style='' id='storyPage' class='bold control'></div>
+                        <button style='' class='actionIcon fa fa-arrow-right control' id='storyRight' onclick='view.updateStory(storyShowing+1)'></button>
+                        ${this.stories()}
+                    </div>
                 </div>
             </div>
         </div>
